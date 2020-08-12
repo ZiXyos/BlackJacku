edfb121b0e4b0bbbaec5f5907aa79b1420096a09
Arthur Bielle <abielle@localhost.localdomain>
2020-08-12 14:37:23 +0200

    cards added

--- server/Models/cards.js
+++ server/Models/cards.js
@@ -1,22 +1,122 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const cardsSchema = new Schema({
-
-    value: {
-        type: Number,
-        required: true
-    },
-    sign: {
-        type: String,
-        required: true
-    },
-    colors: {
-        type: String,
-        required: true
-    },
-});
-
-
-
-module.exports = Card = mongoose.Schema("card", tableSchema);
+function card(value, sign, colors) {
+    const cardsSchema = new Schema({
+      value: {
+          type: Number,
+          required: true
+      },
+      sign: {
+          type: String,
+          required: true
+      },
+      colors: {
+          type: String,
+          required: true
+      },
+    });
+}
+  value = 2;
+  var signe = 1;
+  var couleur = 1;
+  if (signe == 1 && couleur <= 2) {
+    colors = "Rouge";
+    sign = "Carreau";
+    for (var i = 2; i < 15; i++) {
+      if (i == 11) {
+          value--;
+          card["Valet " + "de " + sign] = new card(value, sign, colors);
+      }
+      else if (i == 12)
+          card["Dame " + "de " + sign] = new card(value, sign, colors);
+      else if (i == 13)
+          card["Roi " + "de " + sign] = new card(value, sign, colors);
+      else if (i == 14) {
+          value++;
+          card["As " + "de " + sign] = new card(value, sign, colors);
+      }
+      else {
+          card[i + " de "+ sign] = new card(value, sign, colors);
+          value++;
+      }
+  }
+  couleur++;
+  signe++;
+  value = 2;
+  }
+  if (signe == 2 && couleur <= 2) {
+      colors = "Rouge";
+      sign = "Coeur";
+      for (var i = 2; i < 15; i++) {
+          if (i == 11) {
+              value--;
+              card["Valet " + "de " + sign] = new card(value, sign, colors);
+          }
+          else if (i == 12)
+              card["Dame " + "de " + sign] = new card(value, sign, colors);
+          else if (i == 13)
+              card["Roi " + "de " + sign] = new card(value, sign, colors);
+          else if (i == 14) {
+              value++;
+              card["As " + "de " + sign] = new card(value, sign, colors);
+          }
+          else {
+              card[i + " de "+ sign] = new card(value, sign, colors);
+              value++;
+          }
+      }
+      signe++;
+      value = 2;
+      couleur++;
+      }
+      if (signe == 3 && couleur > 2) {
+          colors = "Noire";
+          sign = "Pique"
+          for (var i = 2; i < 15; i++) {
+              if (i == 11) {
+                  value--;
+                  card["Valet " + "de " + sign] = new card(value, sign, colors);
+              }
+              else if (i == 12)
+                  card["Dame " + "de " + sign] = new card(value, sign, colors);
+              else if (i == 13)
+                  card["Roi " + "de " + sign] = new card(value, sign, colors);
+              else if (i == 14) {
+                  value++;
+                  card["As " + "de " + sign] = new card(value, sign, colors);
+              }
+              else {
+                  card[i + " de "+ sign] = new card(value, sign, colors);
+                  value++;
+              }
+          }
+          signe++;
+          value = 2;
+          }
+          if (signe == 4 && couleur > 2) {
+              colors = "Noire";
+              sign = "Trèfle"
+              for (var i = 2; i < 15; i++) {
+                  if (i == 11) {
+                      value--;
+                      card["Valet " + "de " + sign] = new card(value, sign, colors);
+                  }
+                  else if (i == 12)
+                      card["Dame " + "de " + sign] = new card(value, sign, colors);
+                  else if (i == 13)
+                      card["Roi " + "de " + sign] = new card(value, sign, colors);
+                  else if (i == 14) {
+                      value++;
+                      card["As " + "de " + sign] = new card(value, sign, colors);
+                  }
+                  else {
+                      card[i + " de "+ sign] = new card(value, sign, colors);
+                      value++;
+                  }
+              }
+            //   signe++;
+            //   value = 2;
+              console.log(card);
+            //   return(card(value, sign, colors));
+            }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-
+var cardsSchema = require('./Models/cards');
 const user = require("./Routes/Api/user");
 const createTable = require("./Routes/Api/createTable");
 
@@ -15,16 +15,19 @@ app.use(
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require("./Config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose.connect( db, { useNewUrlParser: true, useUnifiedTopology: true } )
-    .then(() => console.log("MongoDB successfully connected 🧉"))
-    .catch(err => console.log(err));
+.then(() => console.log("MongoDB successfully connected 🧉"))
+.catch(err => console.log(err));
 
 app.use("/user", user);
 app.use("/table", createTable);
+// app.use("/cards", user);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Server running on port http://localhost:${port} 🔥`));
+
+// console.log(cardsSchema);
