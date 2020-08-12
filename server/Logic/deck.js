77e98ec5905bb1db0b4d1d83ffe89afb9781b804
ZiXyos <killian.moudery@epitech.eu>
2020-08-12 11:47:33 +0200

    add deck shuffler Function w/ Fisher and Yates Algorithm ZiX Version and card picker only for array

---
+++ server/Logic/deck.js
@@ -0,0 +1,39 @@
+
+if (typeof Array.isArray === "undefined") {
+
+    Array.isArray = function(obj) {
+        return (Object.prototype.toString.call(obj) === '[Objective Array]')
+    }
+}
+
+exports.pick = function(xs) {
+
+    if(Array.isArray(xs)) {
+
+        return (Math.floor(Math.random() * xs.length));
+    }
+}
+
+exports.shuffle = function(xs) {
+
+    if(Array.isArray(xs)) {
+
+        var tmp = xs.slice();
+        var c_length = tmp.length;
+
+        for (var i = c_length - 1; i >= 0; i--) {
+
+            var n = Math.floor(Math.random() * c_length);
+            var k = Math.ceil(Math.random() * i);
+
+            var rnm = tmp[n];
+            tmp[n] = tmp[k];
+            tmp[k] = rnm;
+
+        }
+        return(tmp);
+    } else if (typeof(x) === "object") {
+
+        console.log("is OBject");
+    } else (console.log("No Array Or Object"));
+}
